e699ac86749cf65059361a1ddbf16999b95ac310
Keith Rome <krome@wintellect.com>
2015-04-21 22:36:34 -0400

    Update to XF 1.4
    Project templates reference 1.3, and forgot to update right away when
    creating this sample

--- TextStylesDemo/TextStylesDemo/TextStylesDemo.Droid/Resources/Resource.Designer.cs
+++ TextStylesDemo/TextStylesDemo/TextStylesDemo.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,8 @@ namespace TextStylesDemo.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::TextStylesDemo.Droid.Resource.String.ApplicationName;
+			global::Xamarin.Forms.Platform.Resource.String.Hello = global::TextStylesDemo.Droid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
@@ -53,6 +55,25 @@ namespace TextStylesDemo.Droid
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f030001
+			public const int ApplicationName = 2130903041;
+			
+			// aapt resource value: 0x7f030000
+			public const int Hello = 2130903040;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
 			{
 			}
 		}
