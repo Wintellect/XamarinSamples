2fc2aa4e944d4c49347f71a7a48b5229ed84da03
Jon <jwood803@users.noreply.github.com>
2015-09-19 09:15:27 -0400

    Add initial build script

---
+++ FakeDemo/build.fsx
@@ -0,0 +1,16 @@
+﻿#r "packages/FAKE/tools/FakeLib.dll"
+open Fake 
+
+
+Target "Test" (fun _ ->
+    trace "Testing stuff..."
+)
+
+Target "Deploy" (fun _ ->
+    trace "Heavy deploy action"
+)
+
+"Test"            // define the dependencies
+   ==> "Deploy"
+
+Run "Deploy"
