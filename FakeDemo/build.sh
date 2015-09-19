574461a791c7c30e672301f464afe822f6782b29
Jon <jwood803@users.noreply.github.com>
2015-09-18 23:50:18 -0400

    Get initial build install script working
    And add nuget to install FAKE

---
+++ FakeDemo/build.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
+  mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion -Prerelease
+fi
+mono packages/FAKE/tools/FAKE.exe build.fsx $@
