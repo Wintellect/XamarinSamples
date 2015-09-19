d4938148ade89957061a7c3fcfc1023199d0f068
Jon <jwood803@users.noreply.github.com>
2015-09-18 23:49:42 -0400

    Update the app to use XAML because...XAML

--- FakeDemo/App.cs
+++ FakeDemo/App.cs
@@ -7,21 +7,7 @@ namespace FakeDemo
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children =
-                    {
-                        new Label
-                        {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
