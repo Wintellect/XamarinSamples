e60ea639f1d7173cb06b79a3b88170b7dde41017
Jon <jwood803@users.noreply.github.com>
2015-09-21 15:16:18 -0400

    Add iOS and Android build targets

--- FakeDemo/build.fsx
+++ FakeDemo/build.fsx
@@ -1,15 +1,47 @@
 #r "packages/FAKE/tools/FakeLib.dll" // 1
 open Fake // 2
+open Fake.XamarinHelper
+
+let buildDir = "FakeDemo/bin/Debug"
+
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
 
 Target "Test" (fun _ -> // 3
     trace "Testing stuff..."
 )
 
-Target "Build" (fun _ ->
-    trace "Heavy build action"
+Target "BuildPcl" (fun _ ->
+    //RestorePackages ()
+
+    !! "FakeDemo.csproj"
+        |> MSBuild "FakeDemo/bin/Debug" "Build"  [ ("Configuration", "Debug"); ("Platform", "Any CPU") ] 
+        |> Log "---PCL build output---"
 )
 
-"Build" // 4
+Target "Build-iOS" (fun _ ->
+    iOSBuild (fun defaults -> 
+        { 
+            defaults with ProjectPath = "iOS/FakeDemo.iOS.csproj"
+                          OutputPath = "iOS/iPhoneSimulator/Debug"
+                          Configuration = "Debug|iPhoneSimulator"
+                          Target = "Build"
+        })
+)
+
+Target "Build-Droid" (fun _ ->
+    !! "Droid/FakeDemo.Droid.csproj"
+        |> MSBuild "Droid/bin/Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ]
+        |> Log "----Android build output----"
+)
+
+"BuildPcl" // 4
+   ==> "Clean"
    ==> "Test"
 
-Run "Test" // 5
+"Build-Droid"
+   ==> "Build-iOS"
+
+
+RunTargetOrDefault "Test" // 5
