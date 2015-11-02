1f190fd1a2c2722d9fd418a864bf7077ae167432
Jon <jwood803@users.noreply.github.com>
2015-11-02 14:28:40 -0500

    Add previously checked in items for running UI tests

--- FakeDemo/build.fsx
+++ FakeDemo/build.fsx
@@ -1,10 +1,13 @@
 #r "packages/FAKE/tools/FakeLib.dll"
+#load "packageHelpers.fsx"
 open Fake
 open Fake.XamarinHelper
 
 let buildDir = "FakeDemo/bin/Debug"
 let testProj = !! "FakeDemo.UnitTests/FakeDemo.UnitTests.csproj"
 let testDll = !! "FakeDemo.UnitTests/bin/Debug/FakeDemo.UnitTests.dll"
+let uiTestProj = !! "UITests/FakeDemo.UITests.csproj"
+let uiTestDll = !! "UITests/bin/Debug/FakeDemo.UITests.dll"
 
 Target "Clean" (fun _ ->
     CleanDir buildDir
@@ -16,6 +19,12 @@ Target "Build-UnitTests" (fun _ ->
         |> Log "---Unit Test build output----"
 )
 
+Target "Build-UITests" (fun _ ->
+    uiTestProj
+        |> MSBuild "UITests\bin\Debug" "Build" [ ("Configuration", "Debug"); ("Platform", "Any CPU") ]
+        |> Log "---UI Test build output----"
+)
+
 Target "Run-UnitTests" (fun _ ->
     testDll |> NUnit ( fun defaults -> 
         { 
@@ -23,6 +32,32 @@ Target "Run-UnitTests" (fun _ ->
                           ToolName = "nunit-console4" 
                           WorkingDir = "FakeDemo.UnitTests\bin\Debug"
                           DisableShadowCopy = true
+        })
+)
+
+Target "Run-AndroidUITests" (fun _ ->
+    let source = PackageHelpers.androidPackage()
+
+    let dest = 
+        filename source.FullName
+        |> sprintf "./RebuyApp.Android.UITests/%s" 
+        |> fileInfo
+        |> PackageHelpers.moveAndroidApk source
+
+    !! @"./**/RebuyApp.Android.UITests/bin/Release/RebuyApp.Android.UITests.dll"
+        |> NUnit (fun defaults -> { defaults with ErrorLevel = DontFailBuild })
+
+    Shell.Exec("adb", "uninstall de.rebuy.android")
+        |> ignore
+
+    DeleteFile dest.FullName
+)
+
+Target "Run-iOSUITests" (fun _ ->
+    uiTestDll |> NUnit ( fun defaults -> 
+        { 
+            defaults with ToolPath = "/Library/Frameworks/Mono.framework/Commands/"
+                          ToolName = "nunit-console4" 
         })
 )
 
@@ -58,6 +93,7 @@ Target "Build-Droid" (fun _ ->
   ==> "Build-Pcl"
   ==> "Build-iOS"
   ==> "Build-Droid"
-  ==> "Run-UnitTests"
+  ==> "Build-UITests"
+  ==> "Run-AndroidUITests"
 
 RunTargetOrDefault "Run-UnitTests"
---
+++ FakeDemo/packageHelpers.fsx
@@ -0,0 +1,17 @@
+#r "packages/FAKE/tools/FakeLib.dll"
+open System.IO
+open Fake
+open Fake.XamarinHelper
+
+let androidPackage() =
+    AndroidPackage (fun defaults ->
+    {
+        defaults with
+            ProjectPath = "Droid/FakeDemo.Droid.csproj"
+    })
+
+let moveAndroidApk (source : FileInfo) (dest : FileInfo) =
+        DeleteFile dest.FullName
+        Rename dest.FullName source.FullName
+
+        dest
